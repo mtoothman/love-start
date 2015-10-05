30c2b49ac5af8a63cedaacc25809ef97bb1ed587
Matt Toothman <mtoothman@users.noreply.github.com>
2015-10-04 22:35:47 -0400

    Load image successfully

--- main.lua
+++ main.lua
@@ -1,7 +1,8 @@
 debug = true
-
+playerImg = nil -- this is just for storage
 function love.load(arg)
-
+    playerImg = love.graphics.newImage('assets/aircrafts/Aircraft_01.png')
+    --we now have an asset ready to be used inside Love
 end
 
 function love.update(dt)
@@ -9,5 +10,5 @@ function love.update(dt)
 end
 
 function love.draw(dt)
-
+  love.graphics.draw(playerImg, 100, 100)
 end
